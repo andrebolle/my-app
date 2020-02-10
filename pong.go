d91d5f71b48355e15a842841643b616af91657c6
Andre Bolle <andrebolle@gmail.com>
2020-02-10 20:45:50 +0000

    draw paddle & ball

--- pong.go
+++ pong.go
@@ -6,13 +6,7 @@ import (
 	"github.com/veandco/go-sdl2/sdl"
 )
 
-// ------------------ UTILS ---------------------
-
-func ifNilPanic(err error) {
-	if err != nil {
-		panic(fmt.Sprintf("%v", err))
-	}
-}
+// ------------------ GLOBALS/STRCUCTS ----------
 
 type col struct {
 	r, g, b byte
@@ -23,6 +17,14 @@ const w, h int32 = 800, 600
 
 // Pixels
 var pixels = make([]byte, w*h*4)
+
+// ------------------ UTILS ---------------------
+
+func ifNilPanic(err error) {
+	if err != nil {
+		panic(fmt.Sprintf("%v", err))
+	}
+}
 
 func fillScreen(c col) {
 	// col{byte(x % mod), byte(y % mod), 0}
@@ -147,6 +149,46 @@ func dda(x1i, y1i, x2i, y2i int32, c col) {
 	}
 }
 
+// ----------------------------------------------------------------------------
+
+type vec2 struct {
+	x, y float32
+}
+
+type ball struct {
+	pos vec2
+	vel vec2
+	rad int32
+	col col
+}
+
+func (ball *ball) draw() {
+	for y := int32(-ball.rad); y < ball.rad; y++ {
+		for x := int32(-ball.rad); x < ball.rad; x++ {
+			if x*x+y*y < ball.rad*ball.rad {
+				putPixel(int32(ball.pos.x)+x, int32(ball.pos.y)+y, ball.col)
+			}
+		}
+	}
+}
+
+type paddle struct {
+	pos  vec2
+	w, h int32
+	col  col
+}
+
+func (paddle *paddle) draw() {
+	startX := int32(paddle.pos.x) - paddle.w/2
+	startY := int32(paddle.pos.y) - paddle.h/2
+
+	for y := int32(0); y < paddle.h; y++ {
+		for x := int32(0); x < paddle.w; x++ {
+			putPixel(startX+x, startY+y, paddle.col)
+		}
+	}
+}
+
 /*
 Only = is the assignment operator.
 := is a part of the syntax of the Short variable declarations clause.
@@ -154,29 +196,32 @@ Only = is the assignment operator.
 
 func main() {
 
-	// Nice names for things
-	var anywhere int32 = sdl.WINDOWPOS_UNDEFINED
-	var useGPU uint32 = sdl.RENDERER_ACCELERATED
-	var rgba uint32 = sdl.PIXELFORMAT_ABGR8888
-
 	err := sdl.Init(sdl.INIT_EVERYTHING)
 	ifNilPanic(err)
 	defer sdl.Quit()
 
 	// Create a window
-	win, err := sdl.CreateWindow("SDL2", anywhere, anywhere, int32(w), int32(h), sdl.WINDOW_SHOWN)
+	var whatever int32 = sdl.WINDOWPOS_UNDEFINED
+	title := "Andre's game of Pong"
+	win, err := sdl.CreateWindow(title, whatever, whatever, int32(w), int32(h), sdl.WINDOW_SHOWN)
 	ifNilPanic(err)
 	defer win.Destroy()
 
 	// Create a renderer
+	var useGPU uint32 = sdl.RENDERER_ACCELERATED
 	ren, err := sdl.CreateRenderer(win, -1, useGPU)
 	ifNilPanic(err)
 	defer ren.Destroy()
 
 	// Create a texture
-	tex, err := ren.CreateTexture(rgba, sdl.TEXTUREACCESS_STREAMING, int32(w), int32(h))
+	tex, err := ren.CreateTexture(sdl.PIXELFORMAT_ABGR8888, sdl.TEXTUREACCESS_STREAMING, int32(w), int32(h))
 	ifNilPanic(err)
 	defer tex.Destroy()
+
+	leftPaddle := paddle{vec2{100, 100}, 20, 100, col{255, 0, 0}}
+	ball := ball{vec2{300., 300.}, vec2{0., 0.}, 20, col{123, 123, 0}}
+
+	// ----------------------------- GAME LOOP -------------------------
 
 	for {
 		// Handle closing the window
@@ -188,15 +233,23 @@ func main() {
 			}
 		}
 
+		leftPaddle.draw()
+		ball.draw()
+
 		sdl.PumpEvents()
-		mx, my, _ /*state*/ := sdl.GetMouseState()
+		// mx, my, _ /*state*/ := sdl.GetMouseState()
+		// fmt.Println(mx, my)
 
-		fmt.Println(mx, my)
-
+		// Update updates the given texture rectangle with new pixel data.
 		tex.Update(nil, pixels, int(w*4))
+
+		// Copy copies all of the texture to the rendering target.
 		ren.Copy(tex, nil, nil)
+
+		// Present updates the screen with any rendering performed since the previous call.
 		ren.Present()
 
+		// Hold on there boy!
 		sdl.Delay(16)
 	}
 
